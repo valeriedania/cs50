9368b895ac852fd585633833f731dee87d1a2b0a
valeriedania <valeriedania@users.noreply.github.com>
2020-11-12 05:37:19 +0000

    automated commit by check50 [check50=True]

--- readability.c
+++ readability.c
@@ -8,7 +8,7 @@
 int main (void)
 {
    char str[100];
-   int letters = 0, words = 1, i = 0, sent = 0, grade = 1;
+   int letters = 0, words = 1, i = 0, sent = 0, grade = 0;
 
         //Prompt user for a text
         string text = get_string("Text:");
@@ -16,10 +16,10 @@ int main (void)
         //Count the number of letters except space
          for(i = 0; i < strlen(text); i++)
           {
-           if (text[i] != ' ' && text[i] != '!' && text[i] != '?' && text[i] != ';' && text[i] != ',' && text[i] != '.')
-
+           if (text[i] != ' ' && text[i] != '-' && text[i] != '!' && text[i] != '?' && text[i] != ';' && text[i] != ',' && text[i] != '.' && text[i] != '\'')
+            {
                letters++;
-             
+            }
            //Check whether current character is a space, new line or tab
            if (text[i] == ' ' || text[i] == '\n' || text[i] == '\t')
            {
@@ -31,11 +31,12 @@ int main (void)
               sent++;
            }
            
+         
           }
           //Print grade level using Coleman Liau index
-          grade = round(0.0588 * 100*(letters/words) - 0.296 * 100*(sent/words) - 15.8);
-
-         //Check if grade is less than 1 or greater than 16
+          grade = round(0.0588 * 100*((float)letters/words) - 0.296 * 100*((float)sent/words) - 15.8);
+          
+          //Check if grade is less than 1 or greater than 16
           if (grade < 1)
           {
               printf("Before Grade 1\n");
@@ -46,6 +47,5 @@ int main (void)
           }
           else
               printf("Grade %i\n", grade);
-        
-
+             
 }
