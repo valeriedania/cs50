6fb9d42755e921d13225797fc9199016ae01c978
valeriedania <valeriedania@users.noreply.github.com>
2020-11-17 03:43:56 +0000

    automated commit by submit50

--- caesar.c
+++ caesar.c
@@ -14,30 +14,30 @@ int main(int argc, string argv[])
     int argv_length, key, plaintext_length;
     string ciphertext;
     
-      //Check single command-line argument
-      if (argc != 2)
-      {
+    //Check single command-line argument
+    if (argc != 2)
+    {
         printf("Usage: ./caesar key\n");
         return 1;
-      }
+    }
       
      
-      //Verify that argv is an int or else convert
-      argv_length = strlen(argv[1]);
-      key = atoi(argv[1]);
-      for (int i = 0; i < argv_length; i++)
+    //Verify that argv is an int or else convert
+    argv_length = strlen(argv[1]);
+    key = atoi(argv[1]);
+    for (int i = 0; i < argv_length; i++)
       {
         if (argv[1][i] < 48 || argv[1][i] > 57)
-      {
-        printf("Usage: ./caesar key\n");
-        return 1;
-      }
-      else 
-      {
-        key = atoi(argv[1]);
-      }
+         {
+            printf("Usage: ./caesar key\n");
+            return 1;
+         }
+        else 
+        {
+             key = atoi(argv[1]);
+        }
 
-    }
+      }
   
  
     //Prompt user for plaintext
@@ -49,9 +49,9 @@ int main(int argc, string argv[])
     printf("ciphertext: ");
     for (int i = 0; plaintext[i] != '\0' ; i++)
     {
-      //check if char is a letter
-      if (isalpha(plaintext[i]) != 0)
-      {
+        //check if char is a letter
+        if (isalpha(plaintext[i]) != 0)
+        {
         //check for uppercase
         if (isupper(plaintext[i]) != 0)
         {
