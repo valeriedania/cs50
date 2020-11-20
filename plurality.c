d958f7c6f5775306e4ac1fbb82558c63347280e7
valeriedania <valeriedania@users.noreply.github.com>
2020-11-20 00:57:56 +0000

    automated commit by check50 [check50=True]

--- plurality.c
+++ plurality.c
@@ -101,7 +101,7 @@ void print_winner(void)
     {
         if (candidates[i].votes == max_votes)
         {
-            printf("Winner(s): %s\n", candidates[i].name);
+            printf("%s\n", candidates[i].name);
         }
     }
     return;
